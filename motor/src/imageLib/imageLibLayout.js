f070e3d5a3bc327e9bee928bbba65cdcf2177def
vnskingmotor <57930370+VnsKing001@users.noreply.github.com>
2021-09-06 10:40:59 +0700

    fix imgLib

--- motor/src/imageLib/imageLibLayout.js
+++ motor/src/imageLib/imageLibLayout.js
@@ -1,27 +1,34 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
+import BespokeHeader from "../bespoke/Component/bespokeHeader";
 import Nav from "../generalLayout/nav";
 import NavDesktop from "../generalLayout/navDesk";
 import PageName from "../generalLayout/pageName";
-import BackgroundHeader from "./imageLib-comps/BackgroundHeader";
-import BarLayout from "./imageLib-comps/BarLayout";
 import DisplayImg from "./imageLib-comps/DisplayImg";
 import Modal from "./imageLib-comps/Modal";
 import Modalreponsive from "./imageLib-comps/Modalreponsive";
-
+const headerData = {
+  background: "linear-gradient(90deg, #773000 0%, #FFC634 100%)",
+  img: {
+    large: "./img/customDesign/banner-xl.png",
+    medium: "./img/customDesign/banner-m.png",
+    small: "",
+  },
+  titles: ["MV AGUSTA", "CUSTOM", "DESIGN"],
+  content:
+    "The MV Agusta warranty offers free towing to the nearest authorised dealer within 100 km in case of breakdown and many other advantages.",
+};
 export default function ImageLibLayout() {
-
-  const [ choose,setChoose ] = useState(false);
+  const [choose, setChoose] = useState(false);
 
   return (
     <>
       <NavDesktop />
       <Nav color="white" />
       <PageName namePage="MV Agusta Wallpapers" />
-      <BackgroundHeader />
-      <BarLayout/>
-      <DisplayImg setChoose={setChoose}/>
-      {choose && <Modal choose={choose} setChoose={setChoose}/>}
-      {choose && <Modalreponsive choose={choose} setChoose={setChoose}/>}
+      <BespokeHeader headerData={headerData} />
+      <DisplayImg setChoose={setChoose} />
+      {choose && <Modal choose={choose} setChoose={setChoose} />}
+      {choose && <Modalreponsive choose={choose} setChoose={setChoose} />}
     </>
   );
 }
