1bacd1f0f2674f8c21f658a95944d70644c035e6
vnskingmotor <57930370+VnsKing001@users.noreply.github.com>
2021-09-14 22:01:28 +0700

    change imgLib header

--- motor/src/imageLib/imageLibLayout.js
+++ motor/src/imageLib/imageLibLayout.js
@@ -7,13 +7,13 @@ import DisplayImg from "./imageLib-comps/DisplayImg";
 import Modal from "./imageLib-comps/Modal";
 import Modalreponsive from "./imageLib-comps/Modalreponsive";
 const headerData = {
-  background: "linear-gradient(90deg, #773000 0%, #FFC634 100%)",
+  background: "#252525",
   img: {
-    large: "./img/customDesign/banner-xl.png",
-    medium: "./img/customDesign/banner-m.png",
+    large: "./img/imageLibLayout/imgMain/banner-xl.png",
+    medium: "./img/imageLibLayout/imgMain/banner-m.png",
     small: "",
   },
-  titles: ["MV AGUSTA", "CUSTOM", "DESIGN"],
+  titles: ["MV AGUSTA", "WALLPAPER"],
   content:
     "The MV Agusta warranty offers free towing to the nearest authorised dealer within 100 km in case of breakdown and many other advantages.",
 };
