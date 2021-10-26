5fbaf79d11a565f19b10c31089902427dcbfcf61
vbachdoan <vbachdoan@gmail.com>
2021-10-26 20:00:57 +0700

    Agusta Apparel shop

---
+++ motor/src/generalLayout/motorPage/category/components/NavShop.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default class NavShop extends React.Component{
+    render(){
+        return(
+            <div id="slide2">
+
+                <div id="wrap_slide2_content">
+                    <h1 id="slide2_title">CITY PACK: SCHIRANNA</h1>
+                    <div id="slide2_button">
+                        <div id="slide2_button_content">
+                            <p>SHOP NOW</p>
+                            <p>SHOP NOW</p>
+                        </div>
+                    </div>
+                </div>
+            
+            </div>
+        )
+    }
+}
